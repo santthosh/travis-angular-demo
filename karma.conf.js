09258673d2ed95dfc272f6985e603a89a3f67383
Santthosh Selvadurai <santthosh@gmail.com>
2013-08-13 17:03:58 -0700

    Attempting to use PhantomJS instead of Chrome and configuring Karma for single run

--- karma.conf.js
+++ karma.conf.js
@@ -46,11 +46,11 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 // If browser does not capture in given timeout [ms], kill it
 captureTimeout = 5000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
-singleRun = false;
+singleRun = true;
